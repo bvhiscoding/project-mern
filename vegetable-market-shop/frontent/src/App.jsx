41307ce79741d3758d650f32c5ebe0c6cef4c2f6
bvhiscoding <buiviethoang1509@gmail.com>
2026-02-04 23:37:47 +0700

    vegetable-market-app : FE ( init FE react + redux + tailwindcss )

---
+++ vegetable-market-shop/frontent/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import CartPage from './pages/CartPage';
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/product/:id" element={<ProductDetailPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/cart" element={<CartPage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ vegetable-market-shop/frontent/src/store/store.js
@@ -0,0 +1,20 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+const rootReducer = combineReducers({});
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export const persistor = persistStore(store);
